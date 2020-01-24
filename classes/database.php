51e2680e38997dea96fa60941c68976e14316f2d
Ella Schwarz <ellaschwarzt@gmail.com>
2020-01-24 15:32:34 +0100

    Added database connection

---
+++ classes/database.php
@@ -0,0 +1,29 @@
+<?php  
+
+/* ELLAS */ $unix_socket = '/Applications/MAMP/tmp/mysql/mysql.sock';
+
+$db   = 'users_db';
+$user = 'root';
+$pass = 'root';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:unix_socket=$unix_socket;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$stmt = $pdo->query('SELECT * FROM users_db.users');
+while ($row = $stmt->fetch() )
+{
+    echo  '<p>' . $row['email'] . '</p>' . "\n";
+}
+
+ ?>
--- index.php
+++ index.php
@@ -31,6 +31,11 @@
       </login-container>
 </main-content>
 
+<?php 
+
+include 'classes/database.php';
+
+?>
 
   <script src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
