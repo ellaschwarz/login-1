acf733f7d07023db7abe9dce939f069b93550f9d
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-01-26 20:23:07 +0100

    added functionality to inserting the user into db after the users inputs are validated.

--- classes/users.php
+++ classes/users.php
@@ -62,9 +62,22 @@ class User extends DataBase
             if ($name != is_string($name) && $name != is_int($name)) {
                 echo "Name is not a string";
                 throw new Exception('$name must be a string!');
+                return false;
             } else {
-                $this->first_name = $name;
-                echo $name;
+                return $this->first_name = $name;
+            }
+        }
+    }
+
+    //Får värden från "setX" funktionerna efter att de har validerats och skickar resultatet till databasen om de ha gått igenom valideringen.
+    public function setUser($set_first_name, $set_last_name, $set_email, $set_password)
+    {
+        if (isset($_POST["submit_btn"])) {
+            if ($set_first_name !== false && $set_last_name !== false && $set_email !== false && $set_password !== false) {
+                $sql = ("INSERT INTO users_db.users(firstname, lastname, email, password) VALUES (?, ?, ?, ?)");
+                $statement = $this->connect()->prepare($sql);
+                $statement->execute([$set_first_name, $set_last_name, $set_email, $set_password]);
+                echo "Successfully inserted user into DB!";
             }
         }
     }
@@ -81,9 +94,9 @@ class User extends DataBase
         if (isset($last)) {
             if (!is_string($last)) {
                 throw new Exception('$name must be a string!');
+                return false;
             } else {
-                $this->last_name = $last;
-                echo $last;
+                return $this->last_name = $last;
             }
         }
     }
@@ -97,8 +110,9 @@ class User extends DataBase
     public function setEmail($email)
     {
         if (isset($email)) {
-            $this->email = $email;
-            echo $email;
+            return $this->email = $email;
+        } else {
+            return false;
         }
     }
     
@@ -110,13 +124,13 @@ class User extends DataBase
 
     public function setPassword($password, $verified_password)
     {
-
         if (isset($password, $verified_password)) {
             if ($password === $verified_password && $password !== "" && $verified_password !== "") {
-                $this->password = $password;
                 echo "passwords matches";
+                return $this->password = $password;
             } else {
                 echo "ERROR: Passwords do not match." . PHP_EOL;
+                return false;
             }
         }
     }
